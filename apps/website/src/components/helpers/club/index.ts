9fe22d5eadccd976fdb0c6e54356afd9a535d6ee
Samuraizan <gainngreen@gmail.com>
2026-04-05 00:04:33 +0530

    Reapply "feat(club): wire up alumni page — The Network replaces old /club"
    This reverts commit 37cbf10ac37179a93049dd79c4d9569d292d0657.

--- apps/website/src/components/helpers/club/index.ts
+++ apps/website/src/components/helpers/club/index.ts
@@ -1,8 +1,17 @@
 import ZoClub from "./ZoClub";
 
+// Legacy exports (kept for backwards compat)
 export { ZoClub };
 export { default as ClubHero } from "./ClubHero";
 export { default as FounderMode } from "./FounderMode";
 export { default as FounderDirectory } from "./FounderDirectory";
 export { default as Leaderboard } from "./Leaderboard";
 export { default as ClubCTA } from "./ClubCTA";
+
+// Alumni page exports
+export { default as NetworkHero } from "./NetworkHero";
+export { default as ZoEffect } from "./ZoEffect";
+export { default as AlumniDirectory } from "./AlumniDirectory";
+export { default as ProgrammingProof } from "./ProgrammingProof";
+export { default as MentorStack } from "./MentorStack";
+export { default as NetworkCTA } from "./NetworkCTA";
--- apps/website/src/pages/club.tsx
+++ apps/website/src/pages/club.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 import { MetaTags, Page } from "../components/common";
 import {
-  ClubHero,
-  FounderMode,
-  FounderDirectory,
-  Leaderboard,
-  ClubCTA,
+  NetworkHero,
+  ZoEffect,
+  AlumniDirectory,
+  ProgrammingProof,
+  MentorStack,
+  NetworkCTA,
 } from "../components/helpers/club";
-import {
-  Communities,
-  CulturalProtocol,
-  Events,
-  Nodes,
-  Partners,
-} from "../components/helpers/membership";
-import { membershipPageData } from "../config";
 
 import { getServerSideProps as getServerSidePropsType } from "next/dist/build/templates/pages";
 
@@ -26,22 +19,18 @@ const Club: React.FC<
   InferGetServerSidePropsType<typeof getServerSidePropsType>
 > = ({ metaData }) => {
   return (
-    <Page className="bg-black !max-w-none !px-0 !pt-0 snap-y snap-mandatory scroll-smooth">
+    <Page className="bg-black !max-w-none !px-0 !pt-0 snap-y snap-proximity scroll-smooth">
       <MetaTags
         title={metaData?.title}
         description={metaData?.description}
         image={metaData?.image}
       />
-      <ClubHero />
-      <FounderMode />
-      <Communities communities={membershipPageData.communities} />
-      <Nodes nodes={membershipPageData.nodes} />
-      <Events events={membershipPageData.events} />
-      <Partners partners={membershipPageData.partners} />
-      <CulturalProtocol />
-      <FounderDirectory />
-      <Leaderboard />
-      <ClubCTA />
+      <NetworkHero />
+      <ZoEffect />
+      <AlumniDirectory />
+      <ProgrammingProof />
+      <MentorStack />
+      <NetworkCTA />
     </Page>
   );
 };
